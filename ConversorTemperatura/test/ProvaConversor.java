3878ea6fa910209639ab2ab8eca3410a12efcb9a
Jorge <Jorge@JORGE>
2016-10-06 17:39:11 +0200

    Merge commit 'bfc21b32f82c9464c35ee0fdf88d6282b8fa9b8a'
    Conflicts:
    	Ejemple2/src/ejemple2/Ejemple2.java

--- ConversorTemperatura/test/ProvaConversor.java
+++ ConversorTemperatura/test/ProvaConversor.java
@@ -12,6 +12,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import conversortemperatura.*;
+import org.junit.Ignore;
 
 /**
  *
@@ -19,20 +20,20 @@ import conversortemperatura.*;
  */
 public class ProvaConversor {
     
-    /*//private Conversor conversor;
-    private static Conversor conversor;
+    //private Conversor conversor;
+    //private static Conversor conversor;
 
     public ProvaConversor() {
     }
 
     @BeforeClass
     public static void setUpClass() {
-        conversor = new Conversor();
+        //conversor = new Conversor();
     }
 
     @AfterClass
     public static void tearDownClass() {
-        conversor = null;
+        //conversor = null;
     }
 
     @Before
@@ -43,21 +44,21 @@ public class ProvaConversor {
     @After
     public void tearDown() {
         //conversor = null;
-    }*/
-
+    }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
+
     @Test
     public final void testCelsiusAFherenheit() throws TemperaturaNoValidaException {
-        //fail ("Sin implementar");
+        fail ("Sin implementar");
         /*Conversor conversor = new Conversor();
         assertEquals(32, conversor.celsiusAFharenheit(0), 0);*/
     }
 
     @Test
     public final void testCelsiusAReamur() throws TemperaturaNoValidaException {
-        //fail ("Sin implementar");
+        fail ("Sin implementar");
         /*Conversor conversor = new Conversor();
         assertEquals(0, conversor.celsiusAReamur(0), 0);*/
     }
@@ -73,13 +74,13 @@ public class ProvaConversor {
     @Test
     public final void testTemperaturaNoValida() throws TemperaturaNoValidaException {
         //fail ("Sin implementar");
-        /*Conversor conversor = new Conversor();
+        Conversor conversor = new Conversor();
         try {
             assertEquals(32, conversor.celsiusAFharenheit(-400), 0);
             fail("Para temperaturas por encima de -273 la prueba debe pasar.");
         } catch (Exception e) {
 
-        }*/
+        }
 
     }
 }
